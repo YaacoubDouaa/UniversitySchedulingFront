528fa57f32e41d8acb628df67e87503812938f22
douaayacoub <132510423+douaayacoub@users.noreply.github.com>
2025-02-16 23:04:57 +0100

    Merge remote-tracking branch 'origin/test' into test

--- src/app/proposition-rattrapage/proposition-rattrapage.component.ts
+++ src/app/proposition-rattrapage/proposition-rattrapage.component.ts
@@ -1,23 +1,41 @@
 import { Component } from '@angular/core';
-import {PropositionDeRattrapage} from '../models/Notifications';
+import { PropositionDeRattrapage } from '../models/Notifications';
+
+import { Seance } from '../models/Seance';
+import {RattrapageService} from '../rattrapage.service';
 
 @Component({
   selector: 'app-proposition-rattrapage',
   standalone: false,
-
   templateUrl: './proposition-rattrapage.component.html',
-  styleUrl: './proposition-rattrapage.component.css'
+  styleUrls: ['./proposition-rattrapage.component.css']
 })
 export class PropositionRattrapageComponent {
   propositions: PropositionDeRattrapage[] = [
-    { id: 1, date: '2025-03-10', reason: 'Maladie', status: 'En attente', enseignantId: 101 },
-    { id: 2, date: '2025-03-12', reason: 'Voyage académique', status: 'En attente', enseignantId: 102 },
+    { id: 1, date: '2025-03-10', reason: 'Maladie', status: 'En attente', enseignantId: 101 ,type:'COURS',name:'',niveau:'ING_1'},
+    { id: 2, date: '2025-03-12', reason: 'Voyage académique', status: 'En attente', enseignantId: 102,type:'COURS',name:'',niveau:'ING_1'},
   ];
 
-  confirmer(id: number) {
-    this.propositions = this.propositions.map(prop =>
-      prop.id === id ? { ...prop, status: 'Confirmé' } : prop
-    );
+  constructor(private rattrapageService: RattrapageService) {}
+
+  confirmer(prop: PropositionDeRattrapage) {
+    if (prop && prop.status === 'En attente') {
+      prop.status = 'Confirmé';
+      const dateObj = new Date(prop.date);
+      const day = dateObj.toLocaleDateString('fr-FR', { weekday: 'long' }).toUpperCase();
+      const time = '10:15-11:45';
+
+      const seance: Seance = {
+        name: prop.name,
+        id: Math.floor(Math.random() * 1000),
+        room: '',
+        type: prop.type,
+        professor: `Enseignant ${prop.enseignantId}`,
+        code: prop.niveau,
+        biWeekly: false
+      };
+      this.rattrapageService.addRattrapageSeance(day, time, seance);
+    }
   }
 
   refuser(id: number) {
@@ -25,6 +43,7 @@ export class PropositionRattrapageComponent {
       prop.id === id ? { ...prop, status: 'Refusé' } : prop
     );
   }
+
   reinitialiser(id: number) {
     this.propositions = this.propositions.map(prop =>
       prop.id === id ? { ...prop, status: 'En attente' } : prop
