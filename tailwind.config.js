d9da247c2891d1b259d83f9b992c801397bf9002
douaayacoub <132510423+douaayacoub@users.noreply.github.com>
2025-02-21 17:24:15 +0100

    Merge remote-tracking branch 'origin/test' into test

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"], // Adjust paths based on your Angular project structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
