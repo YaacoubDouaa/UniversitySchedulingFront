04c843043c5fdf4f6225ff93321734815fb4e3ee
douaayacoub <132510423+douaayacoub@users.noreply.github.com>
2025-05-05 20:00:47 +0200

    organize commit

--- src/app/Auth/auth-module/auth-module.module.ts
+++ src/app/Auth/auth-module/auth-module.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {AuthRoutingModule} from '../auth-routing/auth-routing.module';
-import {LoginComponent} from '../login/login.component';
+import {LoginComponent} from '../../login/login.component';
 
 
 @NgModule({
